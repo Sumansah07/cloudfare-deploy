d6ecb9d1c3e37141547736ee69b0b0f3aeaf7a0e
Suman Sah <152900053+Sumansah07@users.noreply.github.com>
2025-11-20 21:12:50 +0530

    cc

--- worker.ts
+++ worker.ts
@@ -1,12 +1,24 @@
 import { createRequestHandler } from "@remix-run/cloudflare";
-// @ts-ignore - build is generated
-import * as build from "./build/server/index.js";
+// @ts-ignore
+import * as build from "./build/server";
+// @ts-ignore
+import manifestJSON from "__STATIC_CONTENT_MANIFEST";
+
+const assetManifest = JSON.parse(manifestJSON);
 
 const handleRequest = createRequestHandler(build as any);
 
 export default {
     async fetch(request: Request, env: any, ctx: any): Promise<Response> {
         try {
+            // Serve static assets from /assets
+            const url = new URL(request.url);
+            if (url.pathname.startsWith("/assets/")) {
+                // @ts-ignore
+                return await env.ASSETS.fetch(request);
+            }
+
+            // Handle Remix requests
             return await handleRequest(request, {
                 cloudflare: {
                     env,
@@ -17,7 +29,7 @@ export default {
             } as any);
         } catch (error) {
             console.error("Worker error:", error);
-            return new Response("Internal Server Error", { status: 500 });
+            return new Response("Internal Server Error: " + error, { status: 500 });
         }
     },
 };
